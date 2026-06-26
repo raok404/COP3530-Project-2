7d5ebf86e4dac7e3affe3093619c0d2fd5e0f5f3
Kaydence Rao <dencer797@gmail.com>
2026-06-26 11:31:47 -0400

    Create main.cpp and Recipe.h

---
+++ src/Recipe.h
@@ -0,0 +1,6 @@
+#ifndef COP3530_PROJECT_2_RECIPE_H
+#define COP3530_PROJECT_2_RECIPE_H
+class Recipe {
+    // placeholder class for recipe information that we read in from the data set
+};
+#endif
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "SplayTree.h"
+
+int main() {
+    // placeholder for main program
+    SplayTree test = SplayTree();
+    return 0;
+}
