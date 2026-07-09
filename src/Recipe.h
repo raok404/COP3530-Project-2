d3709125d143e623515ea2e791ca6a4c65177c57
Kaydence Rao <dencer797@gmail.com>
2026-07-09 18:10:51 -0400

    Create Recipe and RecipeBook structs with functionality to read in the dataset

--- src/Recipe.h
+++ src/Recipe.h
@@ -1,6 +1,54 @@
 #ifndef COP3530_PROJECT_2_RECIPE_H
 #define COP3530_PROJECT_2_RECIPE_H
-class Recipe {
-    // placeholder class for recipe information that we read in from the data set
+
+#include <nlohmann/json.hpp>
+#include <fstream>
+#include <iostream>
+
+using json = nlohmann::json;
+
+using namespace std;
+
+struct Recipe {
+    string recipeName;
+    vector<string> ingredientList;
+    string instructions;
+
+    Recipe(string name, vector<string> ingredients, string ins) {
+        recipeName = name;
+        ingredientList = ingredients;
+        instructions = ins;
+    }
+};
+
+struct RecipeBook {
+    // RecipeBook book = RecipeBook("../datasets/normalized_recipes.json");
+    // to load the recipes from the json
+
+    vector<Recipe> recipes;
+
+    RecipeBook(string filename) {
+        ifstream jsonFile(filename);
+        json allRecipes = json::parse(jsonFile);
+
+        auto it = allRecipes.begin();
+        std::cout << it.key() << std::endl;
+        std::cout << it.value() << std::endl;
+
+        for (auto& [key, recipe] : allRecipes.items()) {
+            try {
+                if (recipe.contains("title") && recipe.contains("norm_ingredients") && recipe.contains("instructions")) {
+                    recipes.push_back(Recipe(recipe["title"], recipe["norm_ingredients"], recipe["instructions"]));
+                }
+            }
+            catch (...) {
+                cout << "Data missing for recipe :" << key << endl;
+            }
+        }
+    }
+
+    int getNumRecipes() {
+        return recipes.size();
+    }
 };
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,27 @@
 #include "SplayTree.h"
-
+#include <iostream>
+#include <nlohmann/json.hpp>
 int main() {
-    // placeholder for main program
-    SplayTree test = SplayTree();
-    return 0;
+
+    // example for how to read in the data and put it in tree
+
+    // read in json
+     RecipeBook book = RecipeBook("../datasets/normalized_recipes.json");
+     cout << "Read in " << book.getNumRecipes() << " different recipes" << endl;
+
+    // put every recipe into the tree, using its ingredient as the key
+     SplayTree tree;
+     for (Recipe recipe : book.recipes) {
+         for (string ingredient : recipe.ingredientList) {
+             tree.insert(ingredient, recipe);
+         }
+     }
+
+    // search for any recipe that uses chicken
+     SplayTreeNode* test = tree.search("chicken");
+     if (test != nullptr) {
+         cout << test->recipes.size() << endl; // there's 2628 recipes that use "chicken" as an ingredient
+     }
+
+     return 0;
 }
